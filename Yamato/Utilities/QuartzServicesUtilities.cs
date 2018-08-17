81b82bd5343db03c4210c200d73a6ab9197360d5
Sphexator <kristoffer.tunlid@gmail.com>
2018-08-17 22:59:18 +0200

    Added scsheduler factory

---
+++ Yamato/Utilities/QuartzServicesUtilities.cs
@@ -0,0 +1,24 @@
+﻿using Quartz;
+
+namespace Yamato.Utilities
+{
+    public static class QuartzServicesUtilities
+    {
+        public static void StartCronJob<TJob>(IScheduler scheduler, string cron)
+            where TJob : IJob
+        {
+            var jobName = typeof(TJob).FullName;
+
+            var job = JobBuilder.Create<TJob>()
+                .WithIdentity(jobName)
+                .Build();
+
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity($"{jobName}.trigger")
+                .WithSchedule(CronScheduleBuilder.CronSchedule(cron))
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
